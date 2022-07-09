be316b9368c4294fcfe17ae818de2208427be984
upMICSD <up201707400@fe.up.pt>
2022-07-09 22:57:10 +0100

    Parse command line flags

--- test_n.js
+++ test_n.js
@@ -2,7 +2,75 @@ const VL53L1X = require('./vl53l1x');
 const I2C = require('raspi-i2c').I2C;
 const NUM_OF_DATA = 500;
 const NUM_OF_CAL = 50;
+const DEFAULT_OFFSETS_FILE = "./vl53l1x-offsets.json";
+const DEFAULT_XTALKS_FILE = "./vl53l1x-xtalks.json";
 
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+// -n
+const nSensors = (() => {
+    const nSensors = argv.n ?? 1;
+
+    if (typeof nSensors !== 'number' || Number.isNaN(nSensors) || nSensors < 1) {
+        console.error('Invalid number of sensors');
+        process.exit(1);
+    }
+
+    return Math.floor(nSensors);
+})();
+// -i, --init
+const shouldInitAddresses = argv.init ?? argv.i ?? false;
+// --co, --calibrate-offsets
+const shouldCalibrateOffsets = argv["calibrate-offsets"] ?? argv.co ?? false;
+// --of, --offsets-file
+const offsetsFile = argv["offsets-file"] ?? argv.of ?? DEFAULT_OFFSETS_FILE;
+// --cx, --calibrate-xtalks
+const shouldCalibrateXTalks = argv["calibrate-xtalks"] ?? argv.cx ?? false;
+// --xf, --xtalks-file
+const xTalksFile = argv["xtalks-file"] ?? argv.xf ?? DEFAULT_XTALKS_FILE;
+// -v
+const verbose = argv.v ?? false;
+// -x
+const addresses = (() => {
+    let addresses = argv.x;
+
+    if(!addresses) {
+        // no -x was passed, so we'll use the default addresses
+        if(nSensors === 1) {
+            // if it's only one sensor, we'll use the default address 0x29 when we don't
+            // want to init the addresses or 0x2A when we do
+            addresses = shouldInitAddresses ? [0x2A] : [0x29];
+        } else {
+            // if it's more than one sensor, we'll use the default addresses [0x2A, 0x2B, 0x2C, 0x2D, ...]
+            addresses = new Array(nSensors).fill(null).map((_, i) => 0x2A + i);
+        }
+    } else {
+        if(typeof addresses === 'number') {
+            addresses = [addresses];
+        } else if (!Array.isArray(addresses)) {
+            console.error('Invalid address');
+            process.exit(1);
+        }
+
+        if(addresses.length !== nSensors) {
+            console.error('Please provide the correct amount of addresses');
+            process.exit(1);
+        }
+
+        addresses.forEach(address => {
+            if(typeof address !== 'number' || Number.isNaN(address) || address < 1 || address > 0xFF) {
+                console.error(`Invalid address ${address}`);
+                process.exit(1);
+            }
+        });
+    }
+
+    return addresses;
+})();
+
+const log = verbose ? console.log : () => {};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
